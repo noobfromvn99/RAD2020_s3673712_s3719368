c7392511477a37367a762c32ba34c886809def07
noobfromvn99 <s3673712@student.rmit.edu.au>
2020-04-27 01:46:05 +1000

    add Login function and change css

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+  layout "form"
   def new
     @user = User.new
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
-  get '/signup', to: 'users#new'
-  get '/index', to: 'home#index'
+  get    '/login'  ,   to: 'sessions#new'
+  get    '/signup' ,   to: 'users#new'
+  get    '/index'  ,   to: 'home#index'
+  post   '/login'  ,   to: 'sessions#create'
+  delete '/logout' ,   to: 'sessions#destroy'
   resources :users
 end
