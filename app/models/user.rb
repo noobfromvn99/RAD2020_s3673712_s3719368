2429d0cbd5278bd1e28963f6489a81977231f912
noobfromvn99 <s3673712@student.rmit.edu.au>
2020-04-29 22:56:24 +1000

    add reset password

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-    attr_accessor :remember_token
+    attr_accessor :remember_token, :reset_token
     validates :name, presence: true, length: { maximum: 50 }
     #email validation
     VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
@@ -34,6 +34,7 @@ class User < ApplicationRecord
     end
 
     def authenticated?(remember_token)
+        digest = send("#{attribute}_digest")
         return false if remember_digest.nil?
         BCrypt::Password.new(remember_digest).is_password?(remember_token)
     end
@@ -41,5 +42,21 @@ class User < ApplicationRecord
     # Forgets a user.
     def forget
         update_attribute(:remember_digest, nil)
+    end
+
+    # Sets the password reset attributes.
+    def create_reset_digest
+        self.reset_token = User.new_token
+        update_attribute(:reset_digest, User.digest(reset_token))
+        update_attribute(:reset_sent_at, Time.zone.now)
+    end
+
+    # Sends password reset email.
+    def send_password_reset_email
+        UserMailer.password_reset(self).deliver_now
+    end
+
+    def password_reset_expired?
+        reset_sent_at < 2.hours.ago
     end
 end
