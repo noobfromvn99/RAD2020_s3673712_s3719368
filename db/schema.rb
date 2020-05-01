32eb7527478b1d5c7782c1bbc15b2078554be45a
noobfromvn99 <s3673712@student.rmit.edu.au>
2020-05-01 23:20:54 +1000

    fix bug

--- db/schema.rb
+++ db/schema.rb
@@ -20,8 +20,8 @@ ActiveRecord::Schema.define(version: 2020_04_30_134114) do
     t.string "content", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "user_id", null: false
-    t.bigint "topic_id", null: false
+    t.string "user_id"
+    t.bigint "topic_id"
     t.integer "viewed", default: 0
     t.index ["topic_id"], name: "index_posts_on_topic_id"
   end
