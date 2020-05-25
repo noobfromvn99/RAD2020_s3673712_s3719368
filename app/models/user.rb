70d2227a58f34157e2d57a81771b87277f53be82
noobfromvn99 <s3673712@student.rmit.edu.au>
2020-05-25 20:22:25 +1000

    add phone regex

--- app/models/user.rb
+++ app/models/user.rb
@@ -14,7 +14,7 @@ class User < ApplicationRecord
     #phone validation
     VALID_MOBILE_REGEX = /\d/
     validates :mobile, presence: true,
-               length: { minimum:10},
+               length: { minimum:10, maximum: 13},
                format: { with: VALID_MOBILE_REGEX } 
     has_many :posts, dependent: :destroy
     has_many :comments, dependent: :destroy
